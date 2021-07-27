6add3affb4c2f604f93f00f8ac3777682960956f
oxmc <oxmc7769@gmail.com>
2021-07-27 15:12:54 -0700

    Contrib test

--- sources/code/main.ts
+++ sources/code/main.ts
@@ -96,7 +96,7 @@ const chromiumVersion: string = process.versions.chrome;
  * if you're improving the code of this application
  */
 
-let appContributors: Array<string> = [appAuthor];
+let appContributors: Array<string> = [appAuthor + packageJson.contrib.auth];
 
 // Hence GTK allows for tags there on Linux, generate links to website/email
 if (process.platform === "linux") {
